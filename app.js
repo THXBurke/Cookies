f02db9c3e1602feead98697ff902d4d217968705
THXBurke <rachelwheels@gmail.com>
2017-02-15 18:04:37 -0800

    add table data to html

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 'use strict'
+
+
 var hours = ['6Am', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM'];
 var stores = [];
-var hourlyTotals
+var hourlyTotals = [];
+var salesTable = document.getElementById('salesTable');
+
+//constructor function defines a template for the stores
+function Store(minCustomers, maxCustomers, avgTreats, name) {
+  this.minCustomers = minCustomers;
+  this.maxCustomers = maxCustomers;
+  this.avgTreats = avgTreats;
+  this.customersPerHour = [];
+  this.treatsSoldPerHour = [];
+  this.totalTreats = 0;
+  this.name = name;
+
+  this.numCustomersPerHour = function() {
+    for (var i = 0; i < hours.length; i++) {
+      this.customersPerHour.push(Math.floor(Math.random() * (this.maxCustomers - this.minCustomers +1)) + this.minCustomers);
+    }
+  }
+  // this.numTreatsPerHour
+};
